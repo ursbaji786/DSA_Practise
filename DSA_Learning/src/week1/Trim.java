6c7837e0022a26d01effc0029b7fa1ce403de3da
“ursbaji786” <ursbaji@gmail.com>
2023-04-25 06:41:38 +0530

    images

---
+++ DSA_Learning/src/week1/Trim.java
@@ -0,0 +1,22 @@
+package week1;
+
+import java.util.Arrays;
+
+public class Trim {
+
+	public static void main(String[] args) {
+
+		int[] arr = {2,3,4,5,6,7,8};
+		int num = 3;
+
+		int[] output = new int[num];
+		
+		for(int i = 0 ; i < num; i++) {
+			output[i] = arr[i];
+		}
+		
+		//print
+		System.out.println(Arrays.toString(output));
+	}
+
+}
