b2d8ec5e9606232978fdfbbbc4584b4b7114c9ac
“ursbaji786” <ursbaji@gmail.com>
2023-04-26 06:31:22 +0530

    problems

---
+++ DSA_Learning/src/week1/DiamondPattern.java
@@ -0,0 +1,45 @@
+package week1;
+
+public class DiamondPattern {
+	
+	public void myMethod(int input) {
+		int i,j;
+		// to print upper side
+		 for (i=1; i<=input; i++){
+			 
+			 	//for spaces
+		        for (j=1; j<= input-i; j++){
+		           System.out.print(" ");
+		        }
+		        //for stars
+		        for (j=1; j<=i*2-1; j++ ){
+		           System.out.print("* ");
+		        }
+		        System.out.println();
+		  }
+		 
+		 // to print lower side
+				 for (i=input-1; i>0; i--){
+					 
+					//for spaces
+				        for (j=1; j<= input-i; j++){
+				           System.out.print(" ");
+				        }
+				        //for stars
+				        for (j=1; j<=i*2-1; j++ ){
+				           System.out.print("* ");
+				        }
+				        System.out.println();
+				  } 		
+	}
+
+	public static void main(String[] args) {
+		
+		int input = 5; // number of rows
+		DiamondPattern p = new DiamondPattern();
+		p.myMethod(input);
+		
+
+	}
+
+}
