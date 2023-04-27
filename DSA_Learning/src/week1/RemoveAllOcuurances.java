ad98c0293ceff614db30b1c74ed338607ca5bf0d
“ursbaji786” <ursbaji@gmail.com>
2023-04-27 20:23:00 +0530

    solved problems

---
+++ DSA_Learning/src/week1/RemoveAllOcuurances.java
@@ -0,0 +1,47 @@
+package week1;
+
+public class RemoveAllOcuurances {
+
+	public static void main(String[] args) {
+		
+		int[] a = {1,2,3,3,4,5,3,2,3,9};
+		int index = 2;
+		int indexValue = 0;
+		int count =0;
+		
+		
+		
+		
+		// find Index value
+		indexValue = a[index];
+		System.out.println("Index value is :" + indexValue);
+		
+		//find the count of indexValues present in array
+		for (int i = 0; i < a.length; i++) {
+			if(a[i] == indexValue)
+				count = count +1;			
+		}
+		System.out.println("count of index values :" + count);
+		
+		// create another output array with length as inputarray - count
+		
+		int[] output = new int[a.length-count];
+		
+		// move all values other than indexValues to output array
+		
+		int j =0; // output index starts
+		
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] != indexValue) {
+				output[j++] = a[i];
+			}
+			
+		}
+		
+		// print output array
+		for (int i = 0; i < output.length; i++) {
+			System.out.print(output[i]+",");			
+		}
+	}
+
+}
